702c01a5a636c44581de246549bf4f915fa10475
Konstantin Koniukov <konstantin@parsiq.net>
2024-09-04 17:21:31 +0700

    updated basic demo contracts

--- src/demos/basic/BasicDemoL1Callback.sol
+++ src/demos/basic/BasicDemoL1Callback.sol
@@ -2,12 +2,19 @@
 
 pragma solidity >=0.8.0;
 
-contract BasicDemoL1Callback {
+import '../../AbstractCallback.sol';
+
+contract BasicDemoL1Callback is AbstractCallback {
     event CallbackReceived(
         address indexed origin,
         address indexed sender,
         address indexed reactive_sender
     );
+
+    constructor() AbstractCallback(address(0)) payable {
+    }
+
+    receive() external payable {}
 
     function callback(address sender) external {
         emit CallbackReceived(
