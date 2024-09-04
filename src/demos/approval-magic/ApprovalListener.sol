717eaf6a18f5b2be60111869e8b47c88e9d3f139
Konstantin Koniukov <konstantin@parsiq.net>
2024-09-04 18:42:03 +0700

    added Approval contracts

---
+++ src/demos/approval-magic/ApprovalListener.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+pragma solidity >=0.8.0;
+
+import '../../ISubscriptionService.sol';
+import '../../AbstractReactive.sol';
+import './ApprovalService.sol';
+
+contract ApprovalListener is AbstractReactive {
+    uint256 private constant REACTIVE_CHAIN_ID = 0x512578;
+    uint256 private constant SEPOLIA_CHAIN_ID = 11155111;
+    uint256 private constant SUBSCRIBE_TOPIC_0 = 0x1aec2cf998e5b9daa15739cf56ce9bb0f29355de099191a2118402e5ac0805c8;
+    uint256 private constant UNSUBSCRIBE_TOPIC_0 = 0xeed050308c603899d7397c26bdccda0810c3ccc6e9730a8a10c452b522f8edf4;
+    uint256 private constant APPROVAL_TOPIC_0 = 0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925;
+    uint64 private constant CALLBACK_GAS_LIMIT = 1000000;
+
+    address private owner;
+    ApprovalService private approval_service;
+
+    constructor(
+        ApprovalService service_
+    ) {
+        owner = msg.sender;
+        approval_service = service_;
+        bytes memory payload = abi.encodeWithSignature(
+            "subscribe(uint256,address,uint256,uint256,uint256,uint256)",
+            SEPOLIA_CHAIN_ID,
+            service,
+            SUBSCRIBE_TOPIC_0,
+            REACTIVE_IGNORE,
+            REACTIVE_IGNORE,
+            REACTIVE_IGNORE
+        );
+        (bool subscription_result,) = address(service).call(payload);
+        if (!subscription_result) {
+            vm = true;
+        }
+        payload = abi.encodeWithSignature(
+            "subscribe(uint256,address,uint256,uint256,uint256,uint256)",
+            SEPOLIA_CHAIN_ID,
+            service,
+            UNSUBSCRIBE_TOPIC_0,
+            REACTIVE_IGNORE,
+            REACTIVE_IGNORE,
+            REACTIVE_IGNORE
+        );
+        (subscription_result,) = address(service).call(payload);
+        if (!subscription_result) {
+            vm = true;
+        }
+    }
+
+    receive() external payable {}
+
+    modifier callbackOnly(
+        address evm_id
+    ) {
+        require(msg.sender == address(service), 'Callback only');
+        require(evm_id == owner, 'Wrong EVM ID');
+        _;
+    }
+
+    // Methods specific to reactive network contract instance
+
+    function subscribe(
+        address rvm_id,
+        address subscriber
+    ) external rnOnly callbackOnly(rvm_id) {
+        service.subscribe(
+            SEPOLIA_CHAIN_ID,
+            address(0),
+            APPROVAL_TOPIC_0,
+            REACTIVE_IGNORE,
+            uint256(uint160(subscriber)),
+            REACTIVE_IGNORE
+        );
+    }
+
+    function unsubscribe(
+        address rvm_id,
+        address subscriber
+    ) external rnOnly callbackOnly(rvm_id) {
+        service.unsubscribe(
+            SEPOLIA_CHAIN_ID,
+            address(0),
+            APPROVAL_TOPIC_0,
+            REACTIVE_IGNORE,
+            uint256(uint160(subscriber)),
+            REACTIVE_IGNORE
+        );
+    }
+
+    // Methods specific to ReactVM contract instance
+
+    function react(
+        uint256 /* chain_id */,
+        address _contract,
+        uint256 topic_0,
+        uint256 topic_1,
+        uint256 topic_2,
+        uint256 /* topic_3 */,
+        bytes calldata data,
+        uint256 /* block_number */,
+        uint256 /* op_code */
+    ) external vmOnly {
+        if (topic_0 == SUBSCRIBE_TOPIC_0) {
+            bytes memory payload = abi.encodeWithSignature(
+                "subscribe(address,address)",
+                address(0),
+                address(uint160(topic_1))
+            );
+            emit Callback(REACTIVE_CHAIN_ID, address(this), CALLBACK_GAS_LIMIT, payload);
+        } else if (topic_0 == UNSUBSCRIBE_TOPIC_0) {
+            bytes memory payload = abi.encodeWithSignature(
+                "unsubscribe(address,address)",
+                address(0),
+                address(uint160(topic_1))
+            );
+            emit Callback(REACTIVE_CHAIN_ID, address(this), CALLBACK_GAS_LIMIT, payload);
+        } else {
+            (uint256 amount) = abi.decode(data, (uint256));
+            bytes memory payload = abi.encodeWithSignature(
+                "onApproval(address,address,address,address,uint256)",
+                address(0),
+                address(uint160(topic_2)),
+                address(uint160(topic_1)),
+                _contract,
+                amount
+            );
+            emit Callback(SEPOLIA_CHAIN_ID, address(service), CALLBACK_GAS_LIMIT, payload);
+        }
+    }
+}
