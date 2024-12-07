f31be1a870a4e5b4f99824bb70f880e7d177af5a
Konstantin Koniukov <konstantin@parsiq.net>
2024-12-07 17:23:22 +0700

    improved basic reactive formatting

--- src/demos/basic/BasicDemoReactiveContract.sol
+++ src/demos/basic/BasicDemoReactiveContract.sol
@@ -19,19 +19,22 @@ contract BasicDemoReactiveContract is IReactive, AbstractReactive {
     );
 
     uint256 private constant SEPOLIA_CHAIN_ID = 11155111;
-
     uint64 private constant GAS_LIMIT = 1000000;
 
     // State specific to reactive network instance of the contract
-
     address private _callback;
 
     // State specific to ReactVM instance of the contract
-
     uint256 public counter;
 
-    constructor(address _service, address _contract, uint256 topic_0, address callback) {
+    constructor(
+        address _service,
+        address _contract,
+        uint256 topic_0,
+        address callback
+    ) {
         service = ISystemContract(payable(_service));
+
         if (!vm) {
             service.subscribe(
                 SEPOLIA_CHAIN_ID,
@@ -46,7 +49,6 @@ contract BasicDemoReactiveContract is IReactive, AbstractReactive {
     }
 
     // Methods specific to ReactVM instance of the contract
-
     function react(
         uint256 chain_id,
         address _contract,
@@ -59,6 +61,7 @@ contract BasicDemoReactiveContract is IReactive, AbstractReactive {
         uint256 /* op_code */
     ) external vmOnly {
         emit Event(chain_id, _contract, topic_0, topic_1, topic_2, topic_3, data, ++counter);
+
         if (topic_3 >= 0.1 ether) {
             bytes memory payload = abi.encodeWithSignature("callback(address)", address(0));
             emit Callback(chain_id, _callback, GAS_LIMIT, payload);
@@ -66,7 +69,6 @@ contract BasicDemoReactiveContract is IReactive, AbstractReactive {
     }
 
     // Methods for testing environment only
-
     function pretendVm() external {
         vm = true;
     }
