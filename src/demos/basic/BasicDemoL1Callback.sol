339649cf5ba858d609cc736bca38be3bf592ce87
Konstantin Koniukov <konstantin@parsiq.net>
2025-03-04 12:53:50 +0700

    removed value

--- src/demos/basic/BasicDemoL1Callback.sol
+++ src/demos/basic/BasicDemoL1Callback.sol
@@ -11,7 +11,7 @@ contract BasicDemoL1Callback is AbstractCallback {
         address indexed reactive_sender
     );
 
-    constructor() AbstractCallback(address(0)) payable {
+    constructor(address _callback_sender) AbstractCallback(_callback_sender) payable {
     }
 
     function callback(address sender) external {
