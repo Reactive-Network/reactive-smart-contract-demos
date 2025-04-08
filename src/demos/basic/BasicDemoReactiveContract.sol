eccdc228c5a3d3d0e65706d5ed7772ffa5021963
Konstantin Koniukov <konstantin@parsiq.net>
2025-04-08 21:06:00 +0700

    updated basic reactive contract

--- src/demos/basic/BasicDemoReactiveContract.sol
+++ src/demos/basic/BasicDemoReactiveContract.sol
@@ -2,49 +2,79 @@
 
 pragma solidity >=0.8.0;
 
-import '../../../lib/reactive-lib/src/interfaces/IReactive.sol';
-import '../../../lib/reactive-lib/src/abstract-base/AbstractReactive.sol';
 import '../../../lib/reactive-lib/src/interfaces/ISystemContract.sol';
+import '../../../lib/reactive-lib/src/abstract-base/AbstractPausableReactive.sol';
 
-contract BasicDemoReactiveContract is IReactive, AbstractReactive {
+contract BasicDemoReactiveContract is AbstractPausableReactive {
 
-    uint256 public originChainId;
     uint256 public destinationChainId;
+    uint256 public cronTopic;
+    uint256 public topic_0;
     uint64 private constant GAS_LIMIT = 1000000;
-
-    // State specific to reactive network instance of the contract
     address private callback;
 
     constructor(
         address _service,
         uint256 _originChainId,
         uint256 _destinationChainId,
-        address _contract,
-        uint256 topic_0,
+        address _originContract,
+        uint256 _topic_0,
+        uint256 _cronTopic,
         address _callback
     ) payable {
         service = ISystemContract(payable(_service));
 
-        originChainId = _originChainId;
         destinationChainId = _destinationChainId;
+        topic_0 = _topic_0;
+        cronTopic = _cronTopic;
+        callback = _callback;
 
         if (!vm) {
             service.subscribe(
-                originChainId,
-                _contract,
+                _originChainId,
+                _originContract,
                 topic_0,
                 REACTIVE_IGNORE,
                 REACTIVE_IGNORE,
                 REACTIVE_IGNORE
             );
+
+            if (_cronTopic != 0) {
+                service.subscribe(
+                    block.chainid,
+                    address(service),
+                    _cronTopic,
+                    REACTIVE_IGNORE,
+                    REACTIVE_IGNORE,
+                    REACTIVE_IGNORE
+                );
+            }
         }
-        callback = _callback;
     }
 
-    // Methods specific to ReactVM instance of the contract
-    function react(LogRecord calldata log) external vmOnly {
+    function getPausableSubscriptions() internal view override returns (Subscription[] memory) {
+        if (cronTopic == 0) {
+            return new Subscription[](0);
+        }
+        Subscription[] memory result = new Subscription[](1);
+        result[0] = Subscription(
+            block.chainid,
+            address(service),
+            cronTopic,
+            REACTIVE_IGNORE,
+            REACTIVE_IGNORE,
+            REACTIVE_IGNORE
+        );
+        return result;
+    }
 
-        if (log.topic_3 >= 0.01 ether) {
+    function react(LogRecord calldata log) external vmOnly {
+        if (log.topic_0 == topic_0) {
+            if (log.topic_3 >= 0.01 ether) {
+                bytes memory payload = abi.encodeWithSignature("callback(address)", address(0));
+                emit Callback(destinationChainId, callback, GAS_LIMIT, payload);
+            }
+        } else if (log.topic_0 == cronTopic) {
             bytes memory payload = abi.encodeWithSignature("callback(address)", address(0));
             emit Callback(destinationChainId, callback, GAS_LIMIT, payload);
         }
